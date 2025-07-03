933a3328c97c6dde7e926c4b05428d2da91888d3
Andrei Ilisoi <andrei.ilisoi@accesa.eu>
2025-07-03 13:34:22 +0300

    added contact and about sections

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -13,7 +13,25 @@ const HomePage: React.FC = () => {
           </header>
 
           <img src={image} alt="Company Banner" className="banner" />
+          <h2 className="headers">About us</h2>
+          <section className='about'>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet vehicula nisl. In hac habitasse platea dictumst. Maecenas ac ligula in quam malesuada interdum. Pellentesque a eleifend ex, vel vestibulum ante. Suspendisse tincidunt dapibus arcu. Maecenas vitae maximus ex. Etiam vestibulum id nisi nec porta. Quisque mattis ante urna, in lobortis purus ullamcorper quis. Nunc at ex a nisi condimentum feugiat. Phasellus semper lectus ut quam tristique tincidunt. Phasellus consectetur est nec massa hendrerit, nec ullamcorper augue ultricies. Suspendisse aliquet tellus vel tortor cursus fringilla. Phasellus id lectus feugiat leo molestie pellentesque at non arcu. Suspendisse in massa leo. Suspendisse id vehicula ante, at viverra odio. Suspendisse potenti.</p>
+          </section>
 
+          <h2 className='headers'>Contact us</h2>
+          <section className='contacts'>
+            <div className='contact'>
+              <h3>Andrei-Florin Ilisoi</h3>
+              <h2>0746051962</h2>
+            </div>
+            <div className='contact'>
+              <h3>Vlad-Andrei Cojocariu</h3>
+              <h2>0741575993</h2>
+            </div>
+
+          </section>
+        </div>
+        <div className='right'>
           <section className="services">
             <div className="service">
               <h2>Custom Web Design</h2>
@@ -28,9 +46,6 @@ const HomePage: React.FC = () => {
               <p>Optimized for search engines and blazing fast to help you grow online.</p>
             </div>
           </section>
-        </div>
-        <div className='right'>
-          slides
         </div>
       </div>
       
